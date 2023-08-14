c661bc4712ff475b514cf5e2a691979537419fb9
joelfelipe003 <fa4995297@gmail.com>
2023-08-14 00:51:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval( function time() {
+    let DateToday = new Date();
+    let hr = DateToday.getHours();
+    let min = DateToday.getMinutes();
+    let s = DateToday.getSeconds();
+
+        if(hr < 10) hr = '0' + hr
+
+        if(min < 10)min = '0' + min;
+
+        if(s < 10)s = '0' + s
+
+
+    horas.textContent = hr
+    minutos.textContent = min
+    segundos.textContent = s
+})
